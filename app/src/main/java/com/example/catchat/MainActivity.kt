1a3c3ccd29426907b61917ecc293933cbf5a22d8
shawnee <shanpan513@gmail.com>
2022-08-08 14:36:38 -0600

    added a navigation drawer

--- app/src/main/java/com/example/catchat/MainActivity.kt
+++ app/src/main/java/com/example/catchat/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import com.google.android.material.appbar.MaterialToolbar
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +28,21 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         val builder = AppBarConfiguration.Builder(navController.graph)
+
+        //set up drawer navigation, add drawer icon to the toolbar
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        builder.setOpenableLayout(drawer)
+
         val appBarConfiguration = builder.build()
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
         //set up bottom nav bar navigations
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavView.setupWithNavController(navController)
+        //val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+       //bottomNavView.setupWithNavController(navController)
+
+        //link drawer to navigation
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     //inflates the menu in the toolbar
@@ -39,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    //Defines what to do when menu item is selected
+    //Defines what to do when menu item is selected from the toolbar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val navController = findNavController(R.id.fragmentContainerView)
         return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
